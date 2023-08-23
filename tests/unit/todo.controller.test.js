349af98fe0d5fa95e1f37a2b0bd9c4e0474e6a89
baggy102 <rlgh0806@gmail.com>
2023-08-23 21:28:34 +0900

    study : unit-test configure Jest test environment making Jest mock funtion

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,19 @@
 const TodoController = require("../../controller/todo.controller");
+const TodoModel = require("../../model/todo.model");
+const httpMocks = requrie("node-mocks-http");
+
+TodoModel.create = jest.fn();
 
 describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
+  });
+  it("should call TodoModel.create", () => {
+    let req, res, next;
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+    TodoController.createTodo(req, res, next);
+    expect(TodoModel.create).toBeCalled();
   });
 });
